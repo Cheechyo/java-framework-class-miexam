cf4d63b345a0226d0c34fed5ba68d6a88485328d
Cheechyo <kwj9211@gmail.com>
2017-04-21 16:30:09 +0900

    2. N사와 D사에 두 회사에 납품하려고 합니다. (Template Method Pattern, Factory Method Pattern)

--- test/kr/ac/jejunu/ProductDaoTest.java
+++ test/kr/ac/jejunu/ProductDaoTest.java
@@ -15,7 +15,7 @@ public class ProductDaoTest {
         String title = "제주감귤";
         Integer price = 15000;
 
-        ProductDao productDao = new ProductDao();
+        ProductDao productDao = new JejuProductDao();
         Product product = productDao.get(id);
         assertThat(id, is(product.getId()));
         assertThat(title, is(product.getTitle()));
@@ -31,7 +31,37 @@ public class ProductDaoTest {
         product.setId(id);
         product.setTitle(title);
         product.setPrice(price);
-        ProductDao productDao = new ProductDao();
+        ProductDao productDao = new JejuProductDao();
+        productDao.add(product);
+        product = productDao.get(id);
+        assertThat(id, is(product.getId()));
+        assertThat(title, is(product.getTitle()));
+        assertThat(price, is(product.getPrice()));
+    }
+
+    @Test
+    public void hallaGet() throws SQLException, ClassNotFoundException {
+        Long id = 1L;
+        String title = "제주감귤";
+        Integer price = 15000;
+
+        ProductDao productDao = new HallaProductDao();
+        Product product = productDao.get(id);
+        assertThat(id, is(product.getId()));
+        assertThat(title, is(product.getTitle()));
+        assertThat(price, is(product.getPrice()));
+    }
+
+    @Test
+    public void hallaAdd() throws SQLException, ClassNotFoundException {
+        Long id = randomGernarateId();
+        String title = "제주감귤";
+        Integer price = 15000;
+        Product product = new Product();
+        product.setId(id);
+        product.setTitle(title);
+        product.setPrice(price);
+        ProductDao productDao = new HallaProductDao();
         productDao.add(product);
         product = productDao.get(id);
         assertThat(id, is(product.getId()));

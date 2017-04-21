6b3309e62cf572c62a49bd09b0a60020f60a4632
Cheechyo <kwj9211@gmail.com>
2017-04-21 17:23:17 +0900

    14. 각자 생성한 Context를 JdbcTemplate으로 변경해보세요

--- src/kr/ac/jejunu/ProductDao.java
+++ src/kr/ac/jejunu/ProductDao.java
@@ -1,40 +1,51 @@
 package kr.ac.jejunu;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+
 import java.sql.SQLException;
 
 public class ProductDao {
-    JdbcContext jdbcContext;
+    JdbcTemplate jdbcTemplate;
 
-    public JdbcContext getJdbcContext() {
-        return jdbcContext;
+    public JdbcTemplate getJdbcTemplate() {
+        return jdbcTemplate;
     }
 
-    public void setJdbcContext(JdbcContext jdbcContext) {
-        this.jdbcContext = jdbcContext;
+    public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
     }
 
     public Product get(Long id) throws ClassNotFoundException, SQLException {
         Object[] params = {id};
         String sql = "select * from product where id = ?";
-        return jdbcContext.query(sql, params);
+        return jdbcTemplate.query(sql, params, resultSet -> {
+            Product product = null;
+            if (resultSet.next()) {
+                product = new Product();
+                product.setId(resultSet.getLong("id"));
+                product.setTitle(resultSet.getString("title"));
+                product.setPrice(resultSet.getInt("price"));
+            }
+            return product;
+        });
     }
 
     public void add(Product product) throws SQLException, ClassNotFoundException {
         Object[] params = {product.getId(), product.getTitle(), product.getPrice()};
         String sql = "INSERT INTO product VALUES(?, ?, ?)";
-        jdbcContext.update(sql, params);
+        jdbcTemplate.update(sql, params);
     }
 
     public void delete(Product product) throws SQLException {
         Object[] params = {product.getId()};
         String sql = "DELETE FROM product WHERE id = ?";
-        jdbcContext.update(sql, params);
+        jdbcTemplate.update(sql, params);
     }
 
     public void update(Product product) throws SQLException {
         Object[] params = {product.getTitle(), product.getPrice(), product.getId()};
         String sql = "UPDATE product SET title = ?, price = ? WHERE id = ?";
-        jdbcContext.update(sql, params);
+        jdbcTemplate.update(sql, params);
     }
 
 }
